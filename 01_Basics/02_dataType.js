e8494703033043010bcd2d07daa28d5e0ffa50e4
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-09-29 07:12:13 +0000

    dataType

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,38 @@
+// Data Type
+
+console.log("Sum : ",4
+    +9)         // Code should be readable
+
+/* 
+Primitive data Type
+
+String => ""
+Int => number
+boolean => true/false
+bigInt => largeValue
+undefined => undefined
+null => object
+special =>
+
+// Not avaiable char data Type
+
+
+Non
+
+object
+*/
+
+let name = "Saurabh"
+let age = 19
+let boolean = true
+let male = 'y'
+let undefinedDT;
+let placed = undefined
+let nullDT = null;          // Khali / Blank
+
+// Print
+console.table([name, age, boolean, placed, male, undefined, nullDT])
+
+// Checking the dataType
+console.table([typeof name, typeof age, typeof boolean, typeof placed, typeof male, typeof undefinedDT, typeof nullDT])
+console.log(typeof(name))
