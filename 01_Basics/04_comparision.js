18e2764c49709bcf249b9e8054d58be6afc0cf6c
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-09-29 09:01:15 +0000

    comparator

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,29 @@
+// Comparing values with different dataType
+
+console.log(4>6)    
+console.log(6<2)    
+console.log(1==2)  
+console.log(4>=7,"\n\n")   
+
+value = 2;
+console.log(value -= 5,"\n\n")
+
+// different dataType compare
+console.log("2">1)
+console.log(2<"4")
+console.log("2" == "1")
+console.log("2" == 2)
+console.log("2" === 2,"\n\n")
+
+// '==' only compare two value either string/number
+// '===' compare and also compare dataType
+
+// Undefined and null comparison
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0,"\n\n")
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)      // true
