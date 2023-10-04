d39af4d671fec85c16342b0d6b66f4c638179a50
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-10-05 00:32:18 +0530

    Year Calculate

---
+++ Project/01_age_calculator.js
@@ -0,0 +1,15 @@
+// Age Calculator
+
+// YEAR
+
+// Enter Year of Birth
+let doy = 2004;
+
+// Current Date
+let date = new Date();
+// Current Year
+let currentYear = date.getFullYear();
+// Calculating Your age
+let result = currentYear - doy;
+// Print
+console.log(result);
