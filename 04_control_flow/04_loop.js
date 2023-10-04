feb13bad69cfc0a81e1f4480d083b882de7a0672
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-10-04 23:35:35 +0000

    while and do-while loop

---
+++ 04_control_flow/04_loop.js
@@ -0,0 +1,17 @@
+/* --------------------------- Do While, While Loop --------------------------- */
+
+// While Loop
+// Condition define first
+let index = 1;
+while (index <= 10) {
+    console.log(index);
+    index++;
+}
+
+// Do-While Loop
+// Always one time iteration run 
+// after that check the condition
+do {
+    console.log(index);
+    index++;
+} while (index < 10);
