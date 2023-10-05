21921d126bec2dfd6b22434faf03818b37896853
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-10-05 17:17:48 +0530

    loop

--- 04_control_flow/07_forEach.js
+++ 04_control_flow/07_forEach.js
@@ -3,6 +3,12 @@
 // Array
 let myArr = [5, 3, 7, 1, 9];
 
+// Map
+let map = new Map();
+map.set('firstName', "Saurabh");
+map.set('lastName', "Kumar");
+map.set('email', "saurabh@com");
+
 // For-each loop have some prbuild method
 // for accessing array index use the function
 myArr.forEach( function myfn(index) {
@@ -19,3 +25,8 @@ myArr.forEach(myfn);
 
 // Arrow function
 myArr.forEach( (indes) => { console.log(indes); })
+
+// Map in forEach   values
+map.forEach( function myfunction(index) {
+    console.log(index);
+});
